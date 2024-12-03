6fc5cec316f341bc7c8faa2da6eebd306ba808ad
dncmaduro <manhdungtran.vnuuet@gmail.com>
2024-12-04 01:23:39 +0700

    change logo href

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -6,7 +6,7 @@ interface LogoProps {
 
 const Logo = (props: LogoProps) => {
   return (
-    <Link href="/">
+    <Link href="/home">
       <h1
         className={`text-xl font-bold ${props.className} transform-opacity rounded-lg px-2 pt-1 text-violet-500 transition hover:bg-violet-500 hover:text-white`}
       >
